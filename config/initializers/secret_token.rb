ef9e0979e6f92f9619ceab81ebb86aac5aacebf7
Michele Cannito <tigerrosere@gmail.com>
2014-08-12 21:34:22 -0700

    First Events commit. Rspec and capybara gems installed.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Events::Application.config.secret_key_base = '9766faa512df0de6df70a631efe70526f7005a213762ef5bde39f574b15e1af5d69aa127df17ccd9290b1951c4b2557078913e41a7a5ac33c7284725acd5c467'
